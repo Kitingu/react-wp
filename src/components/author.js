a7d69e433fe10a2f543a991aa39661525f512d81
kitingu <benlegendj@gmail.com>
2020-12-22 23:42:15 +0300

    redirect on context reset

--- src/components/author.js
+++ src/components/author.js
@@ -1,23 +1,34 @@
 import React, { useContext } from 'react';
+import { Redirect } from 'react-router-dom';
 import UserContext from '../context/user/userContext';
 import WpApiContent from '../WpApiContent';
 
 const Author = () => {
   const userContext = useContext(UserContext);
-  const { currentUser,userBlogs } = userContext;
-  const avatar = userBlogs[0].x_gravatar
+  const { currentUser, userBlogs } = userContext;
+
   return (
-    <div>
-      <p>{currentUser.name}</p>
-      <p>
-        <WpApiContent content={currentUser.description} />
-      </p>
-  <p>{userBlogs.length}</p>
-      <img src={avatar} alt="asdf" style={{
-          borderRadius:'50%'
-      }}/>
+    <div className="author-container">
+      {currentUser.name ? (
+        <div>
+          <p>{currentUser.name}</p>
+          <p>
+            <WpApiContent content={currentUser.description} />
+          </p>
+          <p>{userBlogs.length}</p>
+          <img
+            src={userBlogs[0].x_gravatar}
+            alt="asdf"
+            style={{
+              borderRadius: '50%',
+            }}
+          />{' '}
+        </div>
+      ) : (
+        <Redirect to="/" />
+      )}
     </div>
   );
-};      
+};
 
 export default Author;
